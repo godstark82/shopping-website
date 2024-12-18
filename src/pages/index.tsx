87f5d65474ec97d60402d5b7c6d6a7802408f284
VENOM LEADER <79461207+godstark82@users.noreply.github.com>
2024-12-18 15:05:11 +0530

    build fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,44 +5,11 @@ import Footer from '@/components/home/footer'
 
 export default function Home () {
 
-  const cards = [
-    {
-      title: 'Product 1',
-      description: 'Description 1',
-      image: '/images/product1.jpg',
-    },
-    {
-      title: 'Product 2',
-      description: 'Description 2',
-      image: '/images/product2.jpg',
-    },
-    {
-      title: 'Product 3',
-      description: 'Description 3',
-      image: '/images/product3.jpg',
-    },
-    {
-      title: 'Product 4',
-      description: 'Description 4',
-      image: '/images/product4.jpg',
-    },
-    {
-      title: 'Product 5',
-      description: 'Description 5',
-      image: '/images/product5.jpg',
-    },
-    {
-      title: 'Product 6',
-      description: 'Description 6',
-      image: '/images/product6.jpg',
-    },
-  ]
-
   return (
     <>
       <Navbar />
       <HomeCarousel />
-      <CardList cards={cards} />
+      <CardList />
       <Footer />
     </>
   )
