2dacf3849d253764589008c50ef8cb7aa05446f4
VENOM LEADER <79461207+godstark82@users.noreply.github.com>
2024-12-19 18:30:00 +0530

    color fixed

--- components/home/card-list.jsx
+++ components/home/card-list.jsx
@@ -15,7 +15,7 @@ export default function CardList({ cards, title, subCategory }) {
     return <>
         <div className="mx-auto px-4 py-8 container">
             <div className="flex justify-between items-center mb-6">
-                <h2 className="font-bold text-2xl text-gray-900">{title}</h2>
+                <h2 className="font-bold text-2xl">{title}</h2>
                 <a href={`/screens/category/${subCategory.id}`} className="font-medium text-blue-600 hover:text-blue-800 dark:hover:text-blue-300 dark:text-blue-400">
                     View More →
                 </a>
